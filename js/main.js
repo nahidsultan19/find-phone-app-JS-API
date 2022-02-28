f5ed74c5aaae74a892e7328d3cb21c615150ab4f
nahidsultan19 <nahidsultan38@gmail.com>
2022-03-01 01:35:42 +0600

    added sensors and others into phone details.

--- js/main.js
+++ js/main.js
@@ -26,13 +26,13 @@ const displayPhones = phones => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card h-100 shadow border rounded-3">
+        <div class="card h-100 shadow border rounded-3 text-center">
             <img src="${phone.image}" class="card-img-top p-2" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">Brand: ${phone.brand}</p>
             </div>
-            <div onclick="phoneDetails('${phone.slug}')" class="btn btn-primary">Phone Details</div>
+            <div onclick="phoneDetails('${phone.slug}')" class="btn btn-primary">${phone.phone_name} (Details) </div>
         </div>
         `;
         phonesContainer.appendChild(div);
@@ -56,8 +56,20 @@ const displayPhoneDetails = id => {
         <img src="${id.image}" class="card-img-top p-2 img-fluid" alt="...">
         <div class="card-body">
             <h5 class="card-title">${id.name}</h5>
-            <p>${id.releaseDate ? id.releaseDate : 'No release date found'}</p>
-            <p class="card-text">Brand: ${id.brand}</p>
+            <p class="card-text">${id.releaseDate ? id.releaseDate : 'No release date found'}</p>
+            <h2>Features:</h2>
+            <div class="list-group">
+                <li class="card-text list-group-item">Storage: ${id.mainFeatures.storage}</li>
+                <li class="card-text list-group-item">DisplaySize: ${id.mainFeatures.displaySize}</li>
+                <li class="card-text list-group-item">ChipSet: ${id.mainFeatures.chipSet}</li>
+                <li class="card-text list-group-item">Sensors: ${id.mainFeatures.sensors}</li>
+                <li class="card-text list-group-item">WLAN: ${id.others.WLAN}</li>
+                <li class="card-text list-group-item">Bluetooth: ${id.others.Bluetooth}</li>
+                <li class="card-text list-group-item">GPS: ${id.others.GPS}</li>
+                <li class="card-text list-group-item">NFC: ${id.others.NFC}</li>
+                <li class="card-text list-group-item">USB: ${id.others.USB}</li>
+                <li class="card-text list-group-item">Radio: ${id.others.Radio}</li>
+            </div>
         </div>
     </div>
     `;
