709cffef9541cc98322a53e71631553142af919c
nahidsultan19 <nahidsultan38@gmail.com>
2022-03-01 00:39:32 +0600

    added phone details.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const loadPhone = () => {
     const searchInput = document.getElementById('search-input');
     const searchText = searchInput.value;
     document.getElementById('phones-container').textContent = '';
+    document.getElementById('phone-details').textContent = '';
     const error = document.getElementById('error')
     if (searchText != 'phone_name') {
         error.innerText = `Don't have in our store `;
@@ -31,9 +32,33 @@ const displayPhones = phones => {
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">Brand: ${phone.brand}</p>
             </div>
-            <div class="btn btn-primary">Phone Details</div>
+            <div onclick="phoneDetails('${phone.slug}')" class="btn btn-primary">Phone Details</div>
         </div>
         `;
         phonesContainer.appendChild(div);
     });
+}
+
+// phone details 
+const phoneDetails = id => {
+    console.log(id)
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhoneDetails(data.data))
+}
+
+const displayPhoneDetails = id => {
+    console.log(id);
+    const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.innerHTML = `
+    <div class="card h-100 shadow border rounded-3">
+        <img src="${id.image}" class="card-img-top p-2 img-fluid" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${id.name}</h5>
+            <p>${id.releaseDate ? id.releaseDate : 'No release date found'}</p>
+            <p class="card-text">Brand: ${id.brand}</p>
+        </div>
+    </div>
+    `;
 }
