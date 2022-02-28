3e8224250514dcf6c48caadae88ca6d4dafcbc2b
nahidsultan19 <nahidsultan38@gmail.com>
2022-02-28 21:37:17 +0600

    added API and console API data.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log('conn')
+const loadPhone = () => {
+    const searchInput = document.getElementById('search-input');
+    const searchText = searchInput.value;
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+        .then(res => res.json())
+        .then(data => console.log(data.data))
+    searchInput.value = '';
+}
